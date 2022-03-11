312fb6fd966dafc1660c10ac44c4829cbcaaa5ba
Sergey <99127426+Savinoldo@users.noreply.github.com>
2022-03-11 18:57:24 +0300

    Revert "Ex2 js basics part2"

--- src/ex2_js-basics-part2/task-02.js
+++
@@ -1,9 +0,0 @@
-function countElements(array) {
-  for (let i = 0; i < array.length; i += 1) {
-    console.log(array[i]);
-  }
-  console.log(`Общее число элементов массива:${array.length}`);
-  return undefined;
-}
-
-module.exports = countElements();
